1ae8c0bf2c80889d8c394846de9cbf7a34127502
Q <kyykyu000@gmail.com>
2020-02-18 15:05:15 +0000

    V02: some changes in Customers.java

--- Week01/src/Restaurant/Customers.java
+++ Week01/src/Restaurant/Customers.java
@@ -5,17 +5,24 @@ public class Customers {
 	private String fname;
 	private String contact;
 	
+	/**
+	 * a specified constructor
+	 * @param fname
+	 * @param contact
+	 */
 	public Customers(String fname, String contact) {
 		
 		this.fname = fname;
 		this.contact = contact;
 	}
 	
+	//fname getter
 	public String getFname() {
 		
 		return this.fname;
 	}
 	
+	//contact getter
 	public String getContact() {
 		
 		return this.contact;
